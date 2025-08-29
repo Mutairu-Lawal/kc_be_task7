b71f463a2d0201b496eaab625ddca1cc3b40912d
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-08-29 07:45:14 +0100

    `Added middleware to measure response time and updated import statements in main.py`

--- Task-2/main.py
+++ Task-2/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+import time
 from database import create_db_and_tables
 from fastapi.middleware.cors import CORSMiddleware
 from routers import products, cart, admin, users
 from contextlib import asynccontextmanager
-from typing import Annotated
-from sqlmodel import Session
 
 
 @asynccontextmanager
@@ -15,6 +15,16 @@ async def lifespan(app: FastAPI):
     # Shutdown code (if any)
 
 app = FastAPI(title="E-Commerce API", lifespan=lifespan)
+
+
+# Middleware to measure response time
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response: Response = await call_next(request)
+    process_time = (time.time() - start_time) * 1000  # ms
+    response.headers["X-Process-Time"] = str(round(process_time, 2))
+    return response
 
 
 # Allow CORS for frontend
